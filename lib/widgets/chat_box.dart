2d41c9e6d61ded29184577a9c42f67e5a7569d52
David Öhman <dohman_92@hotmail.com>
2022-12-15 23:15:34 +0100

    Create chat_box.dart

---
+++ lib/widgets/chat_box.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:instachat/theme/theme.dart';
+
+class ChatBox extends StatefulWidget {
+  const ChatBox.me({super.key}) : isMe = true;
+  const ChatBox.friend({super.key}) : isMe = false;
+
+  final bool isMe;
+
+  @override
+  State<ChatBox> createState() => _ChatBoxState();
+}
+
+class _ChatBoxState extends State<ChatBox> {
+  late final ScrollController _scrollController;
+
+  final String _message = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(widget.isMe ? 'You' : 'Friend', style: ICTheme.typefaces.headline),
+        Container(
+          width: double.infinity,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.grey.withOpacity(0.2),
+          ),
+          child: SingleChildScrollView(
+            controller: _scrollController,
+            child: Text(
+              _message,
+              textAlign: TextAlign.start,
+              style: ICTheme.typefaces.body,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
